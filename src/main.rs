228e097102f3431d42080987236e7a4c9c0de3f1
Oluwatobi <tobiolajide01@gmail.com>
2024-11-30 18:25:41 -0500

    added random number generator

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -11,6 +12,15 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Less than the number!"),
+        Ordering::Greater => println!("Greater than the number!"),
+        Ordering::Equal => println!("Correct!"),
+
+    }
 
     println!("You guessed: {}", guess);
     println!("The secret number is: {secret_number}");
