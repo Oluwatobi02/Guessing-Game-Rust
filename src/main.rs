03777ab9205d2e8989b7b0f3c32203db9c1de2f5
Oluwatobi <tobiolajide01@gmail.com>
2024-11-30 16:02:16 -0500

    creating random number

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
     let mut guess = String::new();
 
     io::stdin()
@@ -10,4 +13,5 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+    println!("The secret number is: {secret_number}");
 }
