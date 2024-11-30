1de7fc1c55cecc82b52a4b421bcb8d82fdb0898b
Oluwatobi <tobiolajide01@gmail.com>
2024-11-30 18:50:46 -0500

    handled input errors

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,31 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    let mut guess = String::new();
-
-    io::stdin()
+    loop {
+        
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        
+        io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Less than the number!"),
-        Ordering::Greater => println!("Greater than the number!"),
-        Ordering::Equal => println!("Correct!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Less than the number!"),
+            Ordering::Greater => println!("Greater than the number!"),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            },
+            
+        }
+        println!("You guessed: {}", guess);
+}
 
-    }
-
-    println!("You guessed: {}", guess);
-    println!("The secret number is: {secret_number}");
 }
